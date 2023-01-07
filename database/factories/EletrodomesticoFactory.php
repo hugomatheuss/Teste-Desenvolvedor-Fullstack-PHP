c218abd7187af3126c09499f99a3c7c669867e92
hugomatheuss <hugo_matheuss@outlook.com>
2023-01-07 13:30:18 -0300

    Criando estrutura para testes

---
+++ database/factories/EletrodomesticoFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Eletrodomestico;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Eletrodomestico>
+ */
+class EletrodomesticoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Eletrodomestico::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ tests/Unit/EletrodomesticoTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Eletrodomestico;
+use PHPUnit\Framework\TestCase;
+
+class EletrodomesticoTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function test_can_list_eletrodomesticos()
+    {
+        
+    }
+
+    public function test_can_create_eletrodomestico()
+    {
+        
+    }
+
+    public function test_can_show_eletrodomestico()
+    {
+        
+    }
+
+    public function test_can_update_eletrodomestico()
+    {
+        
+    }
+
+    public function test_can_delete_eletrodomestico()
+    {
+        
+    }
+}
